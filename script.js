273571380181733490b379440611707b7bbb7f97
jeffersonm03 <jefferson.negociom03@gmail.com>
2021-05-30 11:47:06 -0300

    detecplay

--- script.js
+++ script.js
@@ -25,6 +25,8 @@ video.addEventListener('play', () => {
     document.body.append(canvas)
     const displaySize = { width: video.width, height: video.height }
     faceapi.matchDimensions(canvas, displaySize)
+
+    let neutro,feliz,desgosto,triste,raiva = 0
     setInterval(async () =>{
         const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
         const resizedDetections = faceapi.resizeResults(detections, displaySize)
@@ -32,6 +34,8 @@ video.addEventListener('play', () => {
         faceapi.draw.drawDetections(canvas, resizedDetections)
         faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
         faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-        console.log(detections[0].expressions.Neutro)
+
+        neutro = detections[0].expressions.Neutro
+        console.log(detections[0].expressions)
     }, 400)
 })
